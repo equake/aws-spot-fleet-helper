a8e7a094b99f367abd7ec028b4c15f1def0f8702
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2016-11-25 18:48:00 -0200

    version bump, setup.py adjustments

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-spot-fleet-helper',
-    version='0.0.2',
+    version='0.0.3',
     description='Python library to help launching a fleet of Spot instances within AWS infrastructure',
     packages=find_packages(),
     license='MIT',
     author='Marcos Araujo Sobrinho',
     author_email='marcos@mcmweb.com.br',
     url='https://gitlab.com/equake/aws_spot_fleet_helper-spot-fleet-helper/',
+    download_url='https://pypi.python.org/pypi/aws-spot-fleet-helper',
     keywords=['aws', 'boto3', 'spot-fleet', 'spot', 'fleet', 'amazon web services'],
     long_description=open('README').read(),
     scripts=['mcmweb/aws_spot_fleet_helper/spot_fleet_config.py'],
